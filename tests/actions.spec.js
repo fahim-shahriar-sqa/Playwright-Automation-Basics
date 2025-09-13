879294f2f2f6b88a5c7ff45c2d83e66ecdcabbbd
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-09-13 10:38:39 +0600

    Playwright wait

--- tests/actions.spec.js
+++ tests/actions.spec.js
@@ -3,5 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test('Click', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  await page.waitForTimeout(2000);
   await page.click("//a[@class='getStarted_Sjon']");
+  await page.waitForTimeout(2000);
 });
