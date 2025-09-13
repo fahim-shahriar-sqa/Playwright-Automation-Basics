8e0379e5cb7448c573644465e75aec8d1a960603
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-09-13 11:11:15 +0600

    Checkbox

--- tests/actions.spec.js
+++ tests/actions.spec.js
@@ -19,3 +19,29 @@ test('allClicks', async ({ page }) => {
   await page.waitForTimeout(2000);
 });
 
+test('checkboxes', async ({ page }) => {
+  await page.goto('https://testkru.com/Elements/Checkboxes');
+  await page.waitForTimeout(2000);
+  await page.check("//input[@id='firstSelect1']");
+  await page.waitForTimeout(2000);
+  await page.check("//input[@id='secondSelect2']");
+  await page.waitForTimeout(2000);
+  await page.check("//input[@id='thirdSelect2']");
+  await page.waitForTimeout(2000);
+  await page.check("//input[@id='secondSelect5']");
+  await page.waitForTimeout(2000);
+  await page.check("//input[@id='secondSelect5']");
+  await page.waitForTimeout(2000);
+  await page.check("//body/div[@class='wrapper d-flex align-items-stretch']/div[@id='content']/div/div[@class='mt-2 row text-dark']/div[@class='col-6 form-group active']/form/ul[@id='myUL']/li[1]/input[1]");
+  await page.waitForTimeout(2000);
+  await page.check("//li[normalize-space()='Websites']//input[@type='checkbox']");
+  await page.waitForTimeout(2000);
+  await page.click("//div[@id='content']//div//ul[1]//li[3]//*[name()='svg']");
+  await page.check("//li[normalize-space()='Selenium']//input[@type='checkbox']");
+  await page.waitForTimeout(2000);
+  await page.uncheck("//li[normalize-space()='Selenium']//input[@type='checkbox']");
+  await page.waitForTimeout(2000);
+  await page.uncheck("//li[normalize-space()='Websites']//input[@type='checkbox']");
+  await page.waitForTimeout(2000);
+  await page.uncheck("//li[normalize-space()='Websites']//input[@type='checkbox']");
+});
