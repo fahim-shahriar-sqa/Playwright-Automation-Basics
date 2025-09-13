b6f61e2f2461cfa62ab14350f91a92d3f8ecc174
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-09-13 10:53:10 +0600

    Playwright double click, left and right click

--- tests/actions.spec.js
+++ tests/actions.spec.js
@@ -1,9 +1,30 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('Click', async ({ page }) => {
+test('click', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.waitForTimeout(2000);
   await page.click("//a[@class='getStarted_Sjon']");
+  await page.waitForTimeout(2000);
+});
+
+test('doubleClick', async ({ page }) => {
+  await page.goto('https://testkru.com/Elements/Buttons');
+  await page.waitForTimeout(2000);
+  await page.dblclick("//button[@id='doubleClick']");
+  await page.waitForTimeout(2000);
+});
+
+test('rightClick', async ({ page }) => {
+  await page.goto('https://testkru.com/Elements/Buttons');
+  await page.waitForTimeout(2000);
+  await page.click("//button[@id='rightClick']", {button: 'right'});
+  await page.waitForTimeout(2000);
+});
+
+test('leftClick', async ({ page }) => {
+  await page.goto('https://testkru.com/Elements/Buttons');
+  await page.waitForTimeout(2000);
+  await page.click("//button[@id='leftClick']");
   await page.waitForTimeout(2000);
 });
