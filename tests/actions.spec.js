d54a7eed94c27962d45a25a1759eaac596255b01
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2025-09-13 10:55:35 +0600

    Organize

--- tests/actions.spec.js
+++ tests/actions.spec.js
@@ -8,23 +8,14 @@ test('click', async ({ page }) => {
   await page.waitForTimeout(2000);
 });
 
-test('doubleClick', async ({ page }) => {
+test('allClicks', async ({ page }) => {
   await page.goto('https://testkru.com/Elements/Buttons');
   await page.waitForTimeout(2000);
   await page.dblclick("//button[@id='doubleClick']");
   await page.waitForTimeout(2000);
-});
-
-test('rightClick', async ({ page }) => {
-  await page.goto('https://testkru.com/Elements/Buttons');
-  await page.waitForTimeout(2000);
   await page.click("//button[@id='rightClick']", {button: 'right'});
-  await page.waitForTimeout(2000);
-});
-
-test('leftClick', async ({ page }) => {
-  await page.goto('https://testkru.com/Elements/Buttons');
   await page.waitForTimeout(2000);
   await page.click("//button[@id='leftClick']");
   await page.waitForTimeout(2000);
 });
+
